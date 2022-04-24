debf94e5d1275178414995f20b297c3f586f2de2
Hadiya A Jabbar <hadiyakaludi25@gmail.com>
2022-04-25 03:54:47 +0500

    Add files via upload

---
+++ chap-2.js
@@ -0,0 +1,44 @@
+// Username//
+var Username = "Hadiya";
+
+//My Name//
+var myName = "Hadiya A Jabbar";
+
+//message//
+var message = "Hello World";
+alert(message);
+
+// Student's bio data//
+     var name = "John Doe";
+     var age_num = 15;
+     var age_text = "years old";
+     var age = age_num + age_text;
+     var education = "Certified application developer";
+     alert(name);
+     alert(age);
+     alert(education);
+
+//JS script to show alert//
+   var food = "pizza \n pizz \n piz \n pi \n p"
+   var tastFood = food.toUpperCase();
+   alert(tastFood);
+
+ //Email addres info//
+var myAdress = "My email adress is";
+ var email = "hadiyakaludi25@gmail.com";
+ alert(myAdress+email);
+
+ //Book adress//
+   var book = "A smarter way to learn javascript";
+   var text = "I'm learning javascript from book";
+   var recom_book = "A smarter way to learn javascript " + "I'm learning javascript from book" ;
+   alert(recom_book);
+
+   //write HTML content through javascript//
+   var text = document.getElementById("text").innerHTML = "Yes! I can write HTML content through javascript";
+
+
+   //special characters//
+   var char = "&mdash+&mdash+&mdash+&mdash+&#9824+&#9829+&#9824+&mdash+&mdash+&mdash+&mdash";
+   alert(char);
+ 
